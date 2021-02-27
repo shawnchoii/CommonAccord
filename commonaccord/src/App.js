2217f7584bdb8b601da4f50f0c7ec1b9ab321385
Shawn Choi <49289294+shawnchoii@users.noreply.github.com>
2021-02-27 15:28:14 -0600

    First commit

--- commonaccord/src/App.js
+++ commonaccord/src/App.js
@@ -1,17 +1,27 @@
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
+import {
+  Typography,
+  IconButton,
+  Container, 
+  Grid, 
+  Button,
+  TextField,
+} from "@material-ui/core";
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Bringing the World to Agreement
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="http://www.commonaccord.org/"
           target="_blank"
           rel="noopener noreferrer"
         >
---
+++ commonaccord/src/components/Header.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import { 
+    AppBar,
+    Toolbar,
+    IconButton,
+    List,
+    ListItem,
+    ListItemText,
+    Container } from '@material-ui/core';
+import { Home } from "@material-ui/icons";
+import { makeStyles } from '@material-ui/core/styles';
+import logo from '../logo.png';
+
+const navLinks = [
+  { title: `about us`, path: `/about-us` },
+  { title: `product`, path: `/product` },
+  { title: `blog`, path: `/blog` },
+  { title: `contact`, path: `/contact` },
+  { title: `faq`, path: `/faq` }
+];
+
+const Header = () => {
+    const classes = useStyles();
+  
+    return (
+      <AppBar position="static">
+        <Toolbar>
+          <Container maxWidth="xl" className={classes.navbarDisplayFlex}>
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
+          <img src={logo} alt="logo" width = "125" height="50"/>
+            <List
+              component="nav"
+              aria-labelledby="main navigation"
+              className={classes.navDisplayFlex}
+            >
+              {navLinks.map(({ title, path }) => (
+                <a href={path} key={title} className={classes.linkText}>
+                  <ListItem button>
+                    <ListItemText primary={title} />
+                  </ListItem>
+                </a>
+              ))}
+            </List>
+          </Container>
+        </Toolbar>
+      </AppBar>
+    );
+  };
+
+const useStyles = makeStyles({
+    navbarDisplayFlex: {
+      display: `flex`,
+      alignItems: 'center',
+      justifyContent: 'space-between'
+    },
+    navDisplayFlex: {
+      display: `flex`,
+      justifyContent: `space-evenly`
+    },
+    linkText: {
+      textDecoration: `none`,
+      textTransform: `uppercase`,
+      color: `white`
+    }
+  });
+
+  export default Header;
